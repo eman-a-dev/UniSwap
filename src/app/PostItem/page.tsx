da5c9acc128703c7a661f43c434c9579062e959d
eman-a-dev <emanarif149@gmail.com>
2025-05-09 22:50:57 +0500

    first commit

---
+++ src/app/PostItem/page.tsx
@@ -0,0 +1,10 @@
+import { PostItemForm } from "../components/PostItemForm/PostItemForm";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>This is PostItem page</h1>
+      <PostItemForm />
+    </div>
+  );
+}
---
+++ src/app/components/PostItemForm/PostItemForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const PostItemForm = () => {
+  return (
+    <div>
+        <div className='flex justify-center items-center h-56 bg-gradient-to-r from-amber-500 to-amber-900'>
+            <h1 className='text-center text-amber-300 '>PostItemForm</h1>
+        </div>
+    </div>
+  )
+}
+
