502f9f6b9465cb809b458a0e2d54073a1b675c7b
eman-a-dev <22f-bscs-106@students.duet.edu.pk>
2026-06-29 00:39:54 +0500

    product detail fix

--- src/app/productdetails/[id]/page.tsx
+++ src/app/productdetails/[id]/page.tsx
@@ -1,13 +1,24 @@
-import ProductPage from '../../components/ProductPage/ProductPage';
+import ProductPage from "../../components/ProductPage/ProductPage";
 
 async function getProductById(id: string) {
-  const baseUrl = process.env.NEXTAUTH_URL || 'http://localhost:3000';
-  const res = await fetch(`${baseUrl}/api/products/${id}`, { cache: 'no-store' });
-  if (!res.ok) throw new Error('Product not found');
+  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/products/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) throw new Error("Product not found");
+
   return res.json();
 }
 
-export default async function Detail({ params }: { params: { id: string } }) {
-  const product = await getProductById(params.id);
+export default async function Detail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const product = await getProductById(id);
+
   return <ProductPage product={product} />;
 }
